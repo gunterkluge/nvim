d11aa535e0cd0c4cc50798f18ea5cb743967acb5
Gunter Kluge <gunter.kluge@t-online.de>
2025-05-14 10:50:18 +0200

    optionene eingestellt
    timeoutlen und ttimeoutlen wichtig für vimtex
    
    a

---
+++ lua/config/options.lua
@@ -0,0 +1,42 @@
+vim.g.have_nerd_font = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.showmode = false
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+vim.opt.breakindent = true
+vim.opt.conceallevel = 2
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 450
+vim.opt.ttimeoutlen = 450
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.confirm = true
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+vim.api.nvim_create_augroup('RemoveTrailingWhitespace', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = 'RemoveTrailingWhitespace',
+  pattern = '*',
+  command = '%s/\\s\\+$//e',
+})
