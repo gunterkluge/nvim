08105f21bb1eec11df6c52dafcc298c4e8165316
Gunter Kluge <gunter.kluge@t-online.de>
2025-05-14 10:48:57 +0200

    diverse keymaps hinzugefügt

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+vim.keymap.set('i', 'jk', '<Esc>')
+vim.keymap.set('c', 'jk', '<Esc>')
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+vim.keymap.set('n', '<leader>j', function()
+  require('custom.journal').open_journal_for_today()
+end, { desc = 'Open Journal' })
+vim.keymap.set('n', '<leader>tz', require('zen-mode').toggle, { desc = 'Toggle ZenMode' })
+vim.keymap.set('n', '<leader>tc', function()
+  vim.wo.conceallevel = (vim.wo.conceallevel == 0) and 2 or 0
+end, { desc = 'Toggle conceal' })
