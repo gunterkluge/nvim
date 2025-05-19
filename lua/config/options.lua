048395340ea815794635b6bbae8e6236bceaf970
Gunter Kluge <gunter.kluge@t-online.de>
2025-05-19 21:32:47 +0200

    Ich sehs grad nicht

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -26,17 +26,3 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.confirm = true
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-vim.api.nvim_create_augroup('RemoveTrailingWhitespace', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePre', {
-  group = 'RemoveTrailingWhitespace',
-  pattern = '*',
-  command = '%s/\\s\\+$//e',
-})
