3f378ab434b0dab3bb41afdc05840c833b95504e
Gunter Kluge <gunter.kluge@t-online.de>
2025-05-19 21:30:34 +0200

    markdown filetype

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -27,6 +27,16 @@ vim.opt_local.spelllang = 'de_de'
 vim.opt_local.conceallevel = 2
 vim.opt_local.foldenable = false
 
+-- Verhindert automatische Kommentare bei 'o' oder Enter
+vim.opt_local.formatoptions:remove 'o'
+vim.opt_local.formatoptions:remove 'r'
+
+-- Optionale Ergänzungen:
+-- Automatischer Zeilenumbruch im Editor anzeigen
+
+-- Optional: autoindent behalten, aber smartindent und co. abschalten, wenn's stört
+-- vim.opt_local.autoindent = true
+-- vim.opt_local.smartindent = false
 vim.opt_local.expandtab = true -- Verwenden von Leerzeichen anstelle von Tabulatoren
 vim.opt_local.shiftwidth = 2 -- 2 Leerzeichen pro Tab
 vim.opt_local.softtabstop = 2 -- Wechselt nach 2 Zeichen für die Tabulatorverwendung
