f1b492dc6ef6ea4983d61a655038ee0232eacc39
Gunter Kluge <gunter.kluge@t-online.de>
2026-03-24 18:52:17 +0100

    noch einmal gestreamlined

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -3,7 +3,8 @@ return {
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
   ft = 'markdown',
   -- Vorkompilierte Binary statt npm install (laeuft auf Mac + Windows ohne Node)
-  build = function()
+  build = function(plugin)
+    vim.cmd('source ' .. plugin.dir .. '/autoload/mkdp/util.vim')
     vim.fn['mkdp#util#install']()
   end,
   keys = {
